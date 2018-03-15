9361be76347809e22ad8d7f86cf0bf4f24795736
reyraa <ali@lightcurve.io>
2018-03-16 00:33:08 +0330

    Add getAccount to http

--- src/api/http.js
+++ src/api/http.js
@@ -4,20 +4,39 @@ import Lisk from 'lisk-js'
 const testNet = {
   name: 'Testnet',
   testnet: true,
-  ssl: true,
-  port: 443,
-  code: 1,
+  ssl: false,
+  node: '',
+  randomPeer: true,
+  port: '7000',
+  // port: 443,
+  bannedPeers: [],
 };
 
 const activePeer = Lisk.api(testNet);
 
 export default ({ endpoint, data }) =>
   new Promise((resolve, reject) => {
-    activePeer.sendRequest(`${endpoint.replace(/^\/|\/$/, '')}/`, data, (res) => {
-      if (res.success) {
-        resolve(res);
-      } else {
-        reject(res);
-      }
-    });
+    console.log(endpoint);
+    if (endpoint === 'getAccount') {
+      activePeer.getAccount(data.address, (data) => {
+        if (data.success) {
+          resolve(data.account);
+        } else if (!data.success && data.error === 'Account not found') {
+          resolve({
+            address: data.address,
+            balance: 0,
+          });
+        } else {
+          reject(data);
+        }
+      });
+    } else {
+      activePeer.sendRequest(`${endpoint.replace(/^\/|\/$/, '')}/`, data, (res) => {
+        if (res.success) {
+          resolve(res);
+        } else {
+          reject(res);
+        }
+      });
+    }
   });
